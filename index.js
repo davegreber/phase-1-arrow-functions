a8566762b6038ce8461e90ede38858a1cb7840ed
Dave Greber <davegreber@gmail.com>
2022-09-22 08:44:31 -0400

    commit tree fd54985478765b3f87c138cff903f87eea6dc3c7

---
+++ index.js
@@ -0,0 +1,9 @@
+// has a function expression called divide
+const divide = (a, b) => a / b
+
+divide(1000, 200)
+// has an arrow function called square
+const square = (x) => x * x
+
+// has an arrow function called add
+const add = (a, b) => a + b
